89017a77fe8155d9eda86bf565a24f98c724e432
Mike Ackerman <mike.ackerman@biomark.com>
2020-02-06 11:39:33 -0700

    pulled in some lemhi mcnyset data

--- analysis/CalculatingReddDensities.R
+++ analysis/CalculatingReddDensities.R
@@ -63,36 +63,69 @@ mra_redds = st_read("data/redd/F_G_redds_all_UpTo_2018.shp") %>%
   st_transform(crs = crs(mra_rkm))
 
 # write out cleaned redd data
-st_write(mra_redds, "data/redd/mra_redds.shp",
-         delete_layer = T)
+st_write(mra_redds, "data/redd/mra_redds.shp") 
+         #, delete_layer = T) # to overwrite existing file
 
-####################
-# TEMPERATURE DATA #
-####################
-
-
-##Read in norwest temp stuff##
-# all_norwest <- st_read("data/norwest/NorWeST_temps.shp")
+############################
+# NORWEST TEMPERATURE DATA #
+############################
+# ## read in norwest temp data
+# mra_norwest = st_read("data/norwest/NorWeST_temps.shp") %>%
+#   filter(GNIS_NA %in% c("Lemhi River", "Pahsimeroi River", "Salmon River")) %>%
+#   st_transform(crs = crs(mra_rkm))
 # 
-# mra_norwest <- all_norwest %>%
-#   filter(GNIS_NA == "Lemhi River" |
-#            GNIS_NA == "Pahsimeroi River" |
-#            GNIS_NA == "Salmon River") %>%
-#   st_transform(crs = crs(lemhi_rkm))
-# 
+# # write out cleaned mra norwest data
 # st_write(mra_norwest, "data/norwest/mra_norwest.shp")
+#          #, delete_layer = T) # to overwrite existing file
+# 
+# # read in the cleaned norwest temp data from each watershed
+# lemh_norw = st_read("data/norwest/Lemhi_norw.shp")
+# pahs_norw = st_read("data/norwest/Pah_norw.shp")
+# upsa_norw = st_read("data/norwest/Salmon_norw.shp")
+# 
+# # and bind the above 3 sf norwest temp object
+# mra_norw = lemh_norw %>%
+#   rbind(pahs_norw) %>%
+#   rbind(upsa_norw)
+# 
+# # clean up the previous sf norwest temp objects
+# rm(lemh_norw, pahs_norw, upsa_norw)
 
-Lemhi_norw <- st_read("data/norwest/Lemhi_norw.shp")
-Pah_norw <- st_read("data/norwest/Pah_norw.shp")
-Salmon_norw <- st_read("data/norwest/Salmon_norw.shp")
+############################
+# MCNYSET TEMPERATURE DATA #
+############################
+# read in modeled lemhi data
+lemh_tmp_2011 = st_read("../mra_fish_n_hydro/data/lemhi_temp_shapefiles/Lemhi_2011/Lem_2011_8D_mn.shp") %>%
+  mutate_at(vars(starts_with("Tmn")), list(~na_if(., "-9999"))) %>%
+  rename_at(vars(starts_with("Tmn")), funs(paste0("Tmn_", substring(., 8, 10)))) %>%
+  mutate(year = "2011") %>%
+  dplyr::select(RCAID, year, starts_with("Tmn"))
+
+lemh_tmp_2012 = st_read("../mra_fish_n_hydro/data/lemhi_temp_shapefiles/Lemhi_2012/Lem_2012_8D_mn.shp") %>%
+  mutate_at(vars(starts_with("Tmn")), list(~na_if(., "-9999"))) %>%
+  rename_at(vars(starts_with("Tmn")), funs(paste0("Tmn_", substring(., 8, 10)))) %>%
+  mutate(year = "2012") %>%
+  dplyr::select(RCAID, year, starts_with("Tmn"))
+
+lemh_tmp_2013 = st_read("../mra_fish_n_hydro/data/lemhi_temp_shapefiles/Lemhi_2013/Lem_2013_8D_mn.shp") %>%
+  rename_at(vars(starts_with("X")), funs(paste0("Tmn_", substring(., 4, 6)))) %>%
+  mutate(year = "2013") %>%
+  rename(RCAID = LEM_RCAID) %>%
+  dplyr::select(RCAID, year, starts_with("Tmn"))
+
+# bind the lemhi mcnyset data together
+lemh_mcnyset = lemh_tmp_2011 %>%
+  rbind(lemh_tmp_2012) %>%
+  rbind(lemh_tmp_2013) %>%
+  mutate(watershed = "Lemhi") %>%
+  dplyr::select(watershed, everything())
+
+# clean up lemhi mcnyset data
+rm(lemh_tmp_2011, lemh_tmp_2012, lemh_tmp_2013)
 
 
-mra_norwest <- Lemhi_norw %>%
-  rbind(Pah_norw) %>%
-  rbind(Salmon_norw)
 
 ##Joining temperature and rkm to redd data##
-
 redd_norw_rkm <- mra_redds %>%
   st_join(mra_rkm, 
           join = st_nearest_feature, 
@@ -102,14 +135,12 @@ redd_norw_rkm <- mra_redds %>%
           left = TRUE) 
 
 ##Joining RKM to Temp data##
-
 norw_rkm <- mra_norwest %>%
   st_join(mra_rkm, 
           join = st_nearest_feature,
           left = TRUE)
 
 ##Hist of norwest temps at spawning locations (hab use temp)##
-
 spwn_selection <- ggplot(redd_norw_rkm,aes(x = S36_201,
                                  color = River, 
                                  fill = River)) +
@@ -143,10 +174,7 @@ alltemps_dens <- ggplot(mra_norwest,
 
 alltemps_dens
 
-
-
 ##Plotting Temp/RKM and redd locations##
-
 ggplot(norw_rkm, 
        aes(x = rkm, y = S36_201, group = GNIS_NA)) +
   geom_line(aes(color = GNIS_NA)) +
@@ -156,7 +184,6 @@ ggplot(norw_rkm,
 
 
 ##Making plot for Lemhi only##
-
 Lem_redd_norw <- redd_norw_rkm %>%
   filter(GNIS_NA == "Lemhi River")
 
