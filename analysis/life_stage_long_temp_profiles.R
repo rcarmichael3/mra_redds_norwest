37d4909949cfcfa383afdf020c9c384ca99348b8
Mike Ackerman <mike.ackerman@biomark.com>
2020-02-12 10:53:35 -0700

    finished draft of wtsh, spc, ls plot

--- analysis/life_stage_long_temp_profiles.R
+++ analysis/life_stage_long_temp_profiles.R
@@ -20,9 +20,10 @@ library(sf)
 
 # load all of the prepped data
 load("data/mra_temp_rkm_redd_data_prepped.Rdata")
+rm(mra_norwest, mra_redds)
 
 # trim year off of dates in mra_threshold
-mra_threshold = mra_threshold %>%
+thresh_df = mra_threshold %>%
   mutate(start_date = ymd(start_date) - years(1), # converting to a non-leap year
          end_date = ymd(end_date) - years(1)) %>%
   mutate(start_julian = yday(start_date),
@@ -40,5 +41,88 @@ temp_rkm = mra_mcnyset %>%
   #summarise_at(vars(starts_with("d")), funs(mean))
   summarise_at(vars(starts_with("d")), list(mean))
 
+###############
+# LEMHI RIVER #
+###############
+# set params for a single plot
+wtsd = "Lemhi"
+rkms = 0:91
+spc  = "chinook"
+ls   = "adult_holding"
+
+# thresholds for single species x life_stage
+spc_ls_thresh = thresh_df %>%
+  filter(species == spc,
+         life_stage == ls)
+
+# get names of julians within mra_mcnyset that occur within species x life_stage
+spc_ls_julians = as.vector(paste0("d",unique(spc_ls_thresh$start_julian):unique(spc_ls_thresh$end_julian)))
+spc_ls_julians = spc_ls_julians[spc_ls_julians %in% colnames(mra_mcnyset)]
+
+# trim temp_rkm for watershed and columns in spc_ls_julians
+spc_ls_wtsd_temps = temp_rkm %>%
+  st_drop_geometry() %>%
+  filter(watershed == wtsd) %>%
+  filter(rkm %in% rkms) %>%
+  dplyr::select(watershed, rkm, one_of(spc_ls_julians)) %>%
+  gather(key = "julian", value = "temp_c", c(-watershed, -rkm)) %>%
+  group_by(rkm) %>%
+  summarise(mean_c = mean(temp_c),
+            min_c = min(temp_c),
+            max_c = max(temp_c),
+            sd_c = sd(temp_c),
+            n = n()) %>%
+  ungroup()
+
+# grab rounded min and max temps for plotting
+round_any = function(x, accuracy, f=round){f(x/ accuracy) * accuracy}
+min_temp = round_any(min(spc_ls_wtsd_temps$mean_c), 0.5, f = floor)
+max_temp = round_any(max(spc_ls_wtsd_temps$mean_c), 0.5, f = ceiling)
+
+# extract date_span out of life stage thresholds
+date_span = paste0(str_sub(unique(spc_ls_thresh$start_date), start = -5),
+                   " to ", 
+                   str_sub(unique(spc_ls_thresh$end_date), start = -5))
+
+# extract thresholds and assign as object
+for(i in 1:nrow(spc_ls_thresh)) {
+  assign(as.character(spc_ls_thresh[i,"threshhold"]),
+         as.numeric(spc_ls_thresh[i,"temp_c"]))
+}
+
+# start watershed, species, life_stage plot
+wtsd_spc_ls_p = spc_ls_wtsd_temps %>%
+  ggplot(aes(x = rkm)) +
+  # longitudinal temp profile
+  geom_line(aes(y = mean_c), colour = "grey70") +
+  geom_smooth(aes(y = mean_c),
+              method = "loess",
+              colour = "black",
+              se = F) +
+  # min temp threshold
+  geom_hline(yintercept = minimum, colour = "blue", linetype = "dashed") +
+  # optimum temp range
+  geom_ribbon(aes(ymin = optimum_low,
+                  ymax = optimum_high),
+              fill = "green",
+              alpha = 0.2) +
+  # max temp threshold
+  geom_hline(yintercept = maximum, colour = "red", linetype = "dashed") +
+  # acute temp threshold
+  geom_hline(yintercept = acute, colour = "red") +
+  # set axis breaks and limits
+  scale_x_continuous(breaks = seq(0, max(rkms), by = 5)) +
+  scale_y_continuous(breaks = seq(min_temp, max_temp, by = 0.25)) +
+  coord_cartesian(ylim = c(min_temp, max_temp),
+                  expand = F) +
+  # some formatting
+  theme_bw() +
+  labs(x = "River Kilometer",
+       y = "Temperature (°C)",
+       title = paste0(ls, ", ", date_span)) +
+  theme(plot.title = element_text(hjust = 0.01, vjust = -8, size = 12))
+wtsd_spc_ls_p
+
+## END WATERSHED, SPECIES, LIFE STAGE PLOT
 
          
