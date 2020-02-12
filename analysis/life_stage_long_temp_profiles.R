2281176538f839bf0ebee159791e25cc203f03dc
Mike Ackerman <dirtpathwanderer@gmail.com>
2020-02-12 14:44:17 -0700

    finished draft of life_stage_long_temp_profiles.R

--- analysis/life_stage_long_temp_profiles.R
+++ analysis/life_stage_long_temp_profiles.R
@@ -14,9 +14,12 @@
 #############################################################
 
 ## load necessary libraries
+library(tidyverse)
 library(lubridate)
 library(dplyr)
 library(sf)
+library(stringr)
+library(ggpubr)
 
 # load all of the prepped data
 load("data/mra_temp_rkm_redd_data_prepped.Rdata")
@@ -44,13 +47,14 @@ temp_rkm = mra_mcnyset %>%
 ################################################################################
 # WATERSHED, SPECIES, LIFE STAGE LONGITUDINAL TEMP PROFILES WITH LS THRESHOLDS #
 ################################################################################
-# add mins and max to plot
+lss = c("adult_holding", "spawning", "incubation", "emergence",
+        "summer_parr", "winter_presmolt", "spring_smolt")
 
 # set params for a single plot
 wtsd = "Lemhi"
 rkms = 0:91
 spc  = "chinook"
-ls   = "spring_smolt"
+ls   = "summer_parr"
 
 # thresholds for single species x life_stage
 spc_ls_thresh = thresh_df %>%
@@ -88,8 +92,8 @@ spc_ls_wtsd_temps = temp_rkm %>%
 
 # grab rounded min and max temps for plotting
 round_any = function(x, accuracy, f=round){f(x/ accuracy) * accuracy}
-min_temp = round_any(min(spc_ls_wtsd_temps$mean_c), 0.5, f = floor)
-max_temp = round_any(max(spc_ls_wtsd_temps$mean_c), 0.5, f = ceiling)
+min_temp = round_any(min(spc_ls_wtsd_temps$min_c), 0.5, f = floor)
+max_temp = round_any(max(spc_ls_wtsd_temps$max_c), 0.5, f = ceiling)
 
 # extract date_span out of life stage thresholds
 date_span = paste0(str_sub(unique(spc_ls_thresh$start_date), start = -5),
@@ -106,11 +110,13 @@ for(i in 1:nrow(spc_ls_thresh)) {
 wtsd_spc_ls_p = spc_ls_wtsd_temps %>%
   ggplot(aes(x = rkm)) +
   # longitudinal temp profile
-  geom_line(aes(y = mean_c), colour = "grey60") +
+  geom_line(aes(y = mean_c), colour = "grey40") +
   geom_smooth(aes(y = mean_c),
               method = "loess",
               colour = "black",
               se = F) +
+  geom_line(aes(y = min_c), colour = "red", linetype = "dashed") +
+  geom_line(aes(y = max_c), colour = "red", linetype = "dashed") +
   # min temp threshold
   geom_hline(yintercept = minimum, colour = "blue", linetype = "dashed") +
   # optimum temp range
@@ -124,7 +130,7 @@ wtsd_spc_ls_p = spc_ls_wtsd_temps %>%
   geom_hline(yintercept = acute, colour = "red") +
   # set axis breaks and limits
   scale_x_continuous(breaks = seq(0, max(rkms), by = 5)) +
-  scale_y_continuous(breaks = seq(min_temp, max_temp, by = 0.25)) +
+  scale_y_continuous(breaks = seq(min_temp, max_temp, by = 0.5)) +
   coord_cartesian(ylim = c(min_temp, max_temp),
                   expand = F) +
   # some formatting
@@ -135,4 +141,129 @@ wtsd_spc_ls_p = spc_ls_wtsd_temps %>%
   theme(plot.title = element_text(hjust = 0.01, vjust = -8, size = 12))
 wtsd_spc_ls_p
 
-## END WATERSHED, SPECIES, LIFE STAGE PLOT
+## END SINGLE WATERSHED, SPECIES, LIFE STAGE PLOT
+
+#########################################
+# THE ABOVE FOR ALL SCENARIOS AS A LOOP #
+#########################################
+# set params for a single plot
+wtsds = c("Lemhi", "Pahsimeroi", "Upper Salmon")
+spcs  = c("chinook", "steelhead")
+lss   = c("adult_holding", "spawning", "incubation", "emergence",
+          "summer_parr", "winter_presmolt", "spring_smolt")
+for(wtsd in wtsds) {
+  
+  # set rkms for watershed
+  if(wtsd == "Lemhi")       { rkms = 0:91 }
+  if(wtsd == "Pahsimeroi")  { rkms = 0:85 }
+  if(wtsd == "Upper Salmon"){ rkms = 0:56 }
+  
+  for(spc in spcs) {
+    for(ls in lss) {
+      
+      # thresholds for single species x life_stage
+      spc_ls_thresh = thresh_df %>%
+        filter(species == spc,
+               life_stage == ls)
+      
+      # get names of julians within mra_mcnyset that occur within species x life_stage
+      if(unique(spc_ls_thresh$start_julian) < unique(spc_ls_thresh$end_julian)) {
+        spc_ls_julians = unique(spc_ls_thresh$start_julian):unique(spc_ls_thresh$end_julian)
+      }
+      if(unique(spc_ls_thresh$start_julian) > unique(spc_ls_thresh$end_julian)) {
+        spc_ls_julians = c(unique(spc_ls_thresh$start_julian):365,
+                           0:unique(spc_ls_thresh$end_julian))
+      }
+      spc_ls_julians = paste0("d", str_pad(spc_ls_julians, 3, pad = "0"))
+      
+      # old method
+      # spc_ls_julians = as.vector(paste0("d",unique(spc_ls_thresh$start_julian):unique(spc_ls_thresh$end_julian)))
+      # spc_ls_julians = spc_ls_julians[spc_ls_julians %in% colnames(mra_mcnyset)]
+      
+      # trim temp_rkm for watershed and columns in spc_ls_julians
+      spc_ls_wtsd_temps = temp_rkm %>%
+        st_drop_geometry() %>%
+        filter(watershed == wtsd) %>%
+        filter(rkm %in% rkms) %>%
+        dplyr::select(watershed, rkm, one_of(spc_ls_julians)) %>%
+        gather(key = "julian", value = "temp_c", c(-watershed, -rkm)) %>%
+        group_by(rkm) %>%
+        summarise(mean_c = mean(temp_c),
+                  min_c = min(temp_c),
+                  max_c = max(temp_c),
+                  sd_c = sd(temp_c),
+                  n = n()) %>%
+        ungroup()
+      
+      # grab rounded min and max temps for plotting
+      round_any = function(x, accuracy, f=round){f(x/ accuracy) * accuracy}
+      min_temp = round_any(min(spc_ls_wtsd_temps$min_c), 0.5, f = floor)
+      max_temp = round_any(max(spc_ls_wtsd_temps$max_c), 0.5, f = ceiling)
+      
+      # extract date_span out of life stage thresholds
+      date_span = paste0(str_sub(unique(spc_ls_thresh$start_date), start = -5),
+                         " to ", 
+                         str_sub(unique(spc_ls_thresh$end_date), start = -5))
+      
+      # extract thresholds and assign as object
+      for(i in 1:nrow(spc_ls_thresh)) {
+        assign(as.character(spc_ls_thresh[i,"threshold"]),
+               as.numeric(spc_ls_thresh[i,"temp_c"]))
+      }
+      
+      # start watershed, species, life_stage plot
+      wtsd_spc_ls_p = spc_ls_wtsd_temps %>%
+        ggplot(aes(x = rkm)) +
+        # longitudinal temp profile
+        geom_line(aes(y = mean_c), colour = "grey40") +
+        geom_smooth(aes(y = mean_c),
+                    method = "loess",
+                    colour = "black",
+                    se = F) +
+        geom_line(aes(y = min_c), colour = "red", linetype = "dashed") +
+        geom_line(aes(y = max_c), colour = "red", linetype = "dashed") +
+        # min temp threshold
+        geom_hline(yintercept = minimum, colour = "blue", linetype = "dashed") +
+        # optimum temp range
+        geom_ribbon(aes(ymin = optimum_low,
+                        ymax = optimum_high),
+                    fill = "green",
+                    alpha = 0.2) +
+        # max temp threshold
+        geom_hline(yintercept = maximum, colour = "red", linetype = "dashed") +
+        # acute temp threshold
+        geom_hline(yintercept = acute, colour = "red") +
+        # set axis breaks and limits
+        scale_x_continuous(breaks = seq(0, max(rkms), by = 5)) +
+        scale_y_continuous(breaks = seq(min_temp, max_temp, by = 0.5)) +
+        coord_cartesian(ylim = c(min_temp, max_temp),
+                        expand = F) +
+        # some formatting
+        theme_bw() +
+        labs(x = "River Kilometer",
+             y = "Temperature (°C)",
+             title = paste0(ls, ", ", date_span)) +
+        theme(plot.title = element_text(hjust = 0.01, vjust = -8, size = 12))
+      
+      assign(paste0("wtsd_spc_", ls, "_p") ,wtsd_spc_ls_p)
+    
+    } #  END LIFE STAGE LOOP
+    
+    # EACH WATERSHED X SPECIES (6 combinations)
+    wtsd_spc_p = ggarrange(plotlist = list(wtsd_spc_adult_holding_p,
+                                           wtsd_spc_spawning_p,
+                                           wtsd_spc_incubation_p,
+                                           wtsd_spc_emergence_p,
+                                           wtsd_spc_summer_parr_p,
+                                           wtsd_spc_winter_presmolt_p,
+                                           wtsd_spc_spring_smolt_p),
+                           nrow = 3,
+                           ncol = 3)
+    assign(paste0(wtsd, "_", spc, "_p"), wtsd_spc_p) 
+
+  } # END SPECIES LOOP
+} # END WATERSHED LOOP 
+
+
+
+
